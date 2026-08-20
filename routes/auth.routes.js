31e60987dd3ccacb066e6fb763bb1fb56dba91ce
Almas Khan <khanalmas753@gmail.com>
2026-08-20 11:06:12 +0200

    Fix import case for User model

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,5 +1,5 @@
 const router = require("express").Router();
-const User = require("../models/User.model");
+const User = require("../models/user.model");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const isAuthenticated = require("../middleware/auth.middleware");
