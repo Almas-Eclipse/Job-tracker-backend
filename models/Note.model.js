c49456614b5b0ad53fcb0a27415140c858aae21f
Almas Khan <khanalmas753@gmail.com>
2026-09-06 13:48:03 +0200

    Note model with schema and timestamps

---
+++ models/Note.model.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+
+const noteSchema = new mongoose.Schema(
+  {
+    content: {
+      type: String,
+      required: true
+    },
+    jobApplication: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "JobApplication",
+      required: true
+    },
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true
+    }
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Note", noteSchema);
